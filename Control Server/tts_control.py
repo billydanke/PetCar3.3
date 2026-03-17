b03d1f83b34852b6533fe0f369a84667694e969e
billydanke <johnjo490@gmail.com>
2026-03-16 21:53:54 -0500

    made tts non-blocking of the websocket loop

--- Control Server/tts_control.py
+++ Control Server/tts_control.py
@@ -20,6 +20,8 @@ class TtsController:
         self.logger = logger
         self.state = TtsState()
         self.voice_binary = shutil.which(voice_binary)
+        self._queue: asyncio.Queue[str] = asyncio.Queue()
+        self._worker_task: asyncio.Task[None] | None = None
 
         if self.voice_binary is None:
             self.logger.warning("espeak-ng is not installed or not on PATH; TTS commands will return an error.")
@@ -36,16 +38,25 @@ class TtsController:
             return
 
         self.state.last_text = text
-
-        try:
-            await self._speak_text(text)
-        except Exception:
-            self.logger.exception("TTS playback failed")
-            await websocket.send("error tts-failed")
-            return
-
-        self.logger.info("TTS spoke: %s", text)
+        self._ensure_worker()
+        await self._queue.put(text)
+        self.logger.info("TTS queued: %s", text)
         await websocket.send("t ok")
+
+    def _ensure_worker(self) -> None:
+        if self._worker_task is None or self._worker_task.done():
+            self._worker_task = asyncio.create_task(self._run_worker(), name="tts-worker")
+
+    async def _run_worker(self) -> None:
+        while True:
+            text = await self._queue.get()
+            try:
+                await self._speak_text(text)
+                self.logger.info("TTS spoke: %s", text)
+            except Exception:
+                self.logger.exception("TTS playback failed for queued text: %s", text)
+            finally:
+                self._queue.task_done()
 
     async def _speak_text(self, text: str) -> None:
         process = await asyncio.create_subprocess_exec(
